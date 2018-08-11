7eb5d1af1c50bf45fd2b0d12534becc9e7adfb0f
dusanvel <dusanvel@gmail.com>
2018-08-11 12:04:53 -0400

    Added handler for str type request response (for g21_cmd photo) in Requests_test.py

--- Requests_tests.py
+++ Requests_tests.py
@@ -6,25 +6,29 @@ import json
 #url='http://0.0.0.0:8090/greeting/dusan'
 #url='http://0.0.0.0:8090/g11_cmd'
 
-url='https://fp550api.localtunnel.me/g11_cmd'
-url='https://fp550api.localtunnel.me/g12_cmd'
+url='https://fp550irvas.localtunnel.me/g12_cmd'
+# url='https://fp550irvas.localtunnel.me/g12_cmd'
 response = requests.get(url)
 
-"""
+
 print(response.request.body)
 print(response.request.headers.keys)
 print("aaa")
+print(response.json)
 print (response.headers)
 print (response.content)
 print (response.status_code)
-"""
+
 resp_dict=json.loads(response.text)
-print (resp_dict)
+print (type(resp_dict))
+# print (len(resp_dict.items()))
+if type(resp_dict) is str:
+    print(resp_dict)
+else:
+    data_str = ""
 
-data_str=""
+    for m in resp_dict['recv_pck_Data']:
+        data_str=data_str + chr(m)
 
-for m in resp_dict['recv_pck_Data']:
-    data_str=data_str + chr(m)
-    
-print ("\n Fiscal Printer Current  Date and Time or 'PIB': ",data_str)
+    print ("\n Fiscal Printer Current  Date and Time or 'PIB': ",data_str)
 
